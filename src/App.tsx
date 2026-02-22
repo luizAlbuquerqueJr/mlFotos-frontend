277b24cb53a0a95e600eedbb40d8544a5d470146
Luiz Albuquerque <laaj@cin.ufpe.br>
2026-02-22 19:05:29 -0300

    fix: github action

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/mlFotos-frontend">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
